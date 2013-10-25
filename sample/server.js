713e488884e69c01f25308f242c74d3d6f0c2b94
mjeanroy <mickael.jeanroy@mos.gfit.fr>
2013-10-25 17:39:55 +0200

    sample: add custom autocomplete in sample

--- sample/server.js
+++ sample/server.js
@@ -3,6 +3,18 @@ var qs = require('querystring');
 
 var app = express();
 
+var elephant = {
+  id: 1,
+  name: 'Elephant'
+};
+
+var horse = {
+  id: 2,
+  name: 'Horse'
+};
+
+var species = [elephant, horse];
+
 app.get('/', function(req, res) {
   res.sendfile('./sample/index.html');
 });
@@ -25,6 +37,43 @@ app.post('/tags', function(req, res) {
       height: POST.height
     };
     res.json(tag);
+  });
+});
+
+app.get('/species', function(req, res) {
+  var query = req.query;
+  var filter = (query.filter || '').toLowerCase();
+
+  var results = [];
+  for (var i = 0, ln = species.length; i < ln; ++i) {
+    var sp = species[i];
+    if (sp.name.toLowerCase().indexOf(filter) >= 0) {
+      results.push(sp);
+    }
+  }
+
+  res.json(results);
+});
+
+app.post('/species', function(req, res) {
+  var body = '';
+  req.on('data', function (data) {
+    body += data;
+  });
+
+  req.on('end', function () {
+    var POST = qs.parse(body);
+
+    var newId = species[species.length - 1].id;
+    newId++;
+
+    var sp = {
+      id: newId,
+      name: POST.name
+    };
+ 
+    species.push(sp);
+    res.json(sp);
   });
 });
 
